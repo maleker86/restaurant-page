a49dbf08a2f8e5a8e1d8b41e51634b6f1923e4fd
Kris <109919530+maleker86@users.noreply.github.com>
2024-01-06 19:56:58 -0800

    Remove hard-coded & prepate to link JS via Webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-console.log("Everything is in ship-shape.")
+console.log("this is the big index.js.");
---
+++ src/page-load.js
@@ -0,0 +1,7 @@
+console.log("This is specifically the page-load.js");
+
+{/* <div id="content">
+        <img src="https://images.unsplash.com/photo-1600891964599-f61ba0e24092?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="https://unsplash.com/photos/sliced-bread-on-white-ceramic-plate-STqHLqMne3k" class="header">
+        <h2 class="header-text">Welcome to the Restaurant!</h2>
+        <p>some copy about how wonderful the restaurant is. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac tortor odio. Duis ultricies dapibus massa, at auctor erat ultricies in. Proin et lacus varius. 
+       </p> */}
