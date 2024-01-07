626398736d3b9af82000e21131e30957c88219a6
Kris <109919530+maleker86@users.noreply.github.com>
2024-01-06 19:59:10 -0800

    IMPORT PAGE-LOAD.JS INTO INDEX.JS

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-console.log("Everything is in ship-shape.");
+(()=>{var e={204:()=>{console.log("This is specifically the page-load.js")}},r={};function o(t){var s=r[t];if(void 0!==s)return s.exports;var i=r[t]={exports:{}};return e[t](i,i.exports,o),i.exports}o.n=e=>{var r=e&&e.__esModule?()=>e.default:()=>e;return o.d(r,{a:r}),r},o.d=(e,r)=>{for(var t in r)o.o(r,t)&&!o.o(e,t)&&Object.defineProperty(e,t,{enumerable:!0,get:r[t]})},o.o=(e,r)=>Object.prototype.hasOwnProperty.call(e,r),(()=>{"use strict";o(204),console.log("this is the big index.js.")})()})();
